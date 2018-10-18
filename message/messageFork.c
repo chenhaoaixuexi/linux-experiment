0038681b70c2f9375290d78c104352c232107627
chenhaoaixuexi <2630601704@qq.com>
2018-10-18 15:48:54 +0800

    message fork

---
+++ message/messageFork.c
@@ -0,0 +1,67 @@
+/*{{{
+ * =====================================================================================
+ *
+ *       Filename:  messageFork.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/18/18 15:15:39
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ *//*}}}*/
+
+/*{{{*/
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <stdio.h>/*}}}*/
+
+#define MSGKEY 75 
+
+struct msgform
+{
+	long mtype;
+	char mtext[1000];
+}msg;
+
+int msgqid;
+int pid;
+
+int main(int argc, char *argv[])
+{
+	while(-1 == (pid = fork()))	;
+	if (pid > 0 )// in parent
+	{
+		msgqid = msgget(MSGKEY, 0777|IPC_CREAT);
+		do 
+		{
+			msgrcv(msgqid,&msg,1024,0,0);
+			printf("(server)received\n");
+		}while(msg.mtype!=1);
+		msgctl(msgqid,IPC_RMID,0);
+		wait(NULL);
+		exit(0);
+	}
+	else// in child
+	{
+		int i;
+		msgqid = msgget(MSGKEY,0777);
+		for(i= 10;i>=1;i++)
+		{
+			msg.mtype = i;
+			printf("(client)sent\n");
+			msgsnd(msgqid,&msg,1024,0);
+		}
+		exit(0);
+	}
+	return 0;
+}
